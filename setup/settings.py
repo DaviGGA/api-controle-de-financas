7cde2b932844fad4b6679e4615455721b08abce2
DaviGGA <davigregorio.a@hotmail.com>
2022-08-17 20:51:52 -0300

    Ajustes

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mrr1^nw6799fnke(lmka*6zb5$v-ecm=pv2a7h^ki(d4rkj1ts
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
