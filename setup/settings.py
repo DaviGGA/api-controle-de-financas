7f46e0e938954456422ea6bcb6f2c9cf369ea0ad
DaviGGA <davigregorio.a@hotmail.com>
2022-08-17 21:18:16 -0300

    Ajustes

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mrr1^nw6799fnke(lmka*6zb5$v-ecm=pv2a7h^ki(d4rkj1ts'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
