6c787468c16ba1a72ae5584cf454937c9d8fc8bb
DaviGGA <davigregorio.a@hotmail.com>
2022-08-18 16:30:52 -0300

    Deploy

--- setup/urls.py
+++ setup/urls.py
@@ -3,21 +3,21 @@ from django.urls import path, include
 from rest_framework import routers
 from api.views import *
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+# from drf_yasg.views import get_schema_view
+# from drf_yasg import openapi
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="API Financeira",
-      default_version='v1',
-      description="Registro de Receitas e Despesas",
-      terms_of_service="#",
-      contact=openapi.Contact(email="davigregorio.a@hotmail.com"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+# schema_view = get_schema_view(
+#    openapi.Info(
+#       title="API Financeira",
+#       default_version='v1',
+#       description="Registro de Receitas e Despesas",
+#       terms_of_service="#",
+#       contact=openapi.Contact(email="davigregorio.a@hotmail.com"),
+#       license=openapi.License(name="BSD License"),
+#    ),
+#    public=True,
+#    permission_classes=[permissions.AllowAny],
+# )
 
 router = routers.DefaultRouter()
 
@@ -32,7 +32,7 @@ urlpatterns = [
     path('despesas/<int:month>/<int:year>',ListaDespesas.as_view()),
     path('resumo/<int:mes>/<int:ano>', ResumoDoMes.as_view()),
     path('registro/',UsuarioRegistroViewSet.as_view()),
-    path('documentacao/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   #  path('documentacao/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    
 
 ]
