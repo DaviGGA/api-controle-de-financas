9f25035f8b75f574c905d335e06506e8e128ffda
DaviGGA <davigregorio.a@hotmail.com>
2022-08-18 15:36:16 -0300

    Deploy

--- setup/settings.py
+++ setup/settings.py
@@ -126,6 +126,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
